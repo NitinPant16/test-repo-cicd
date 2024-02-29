b718c034783a7643d89cd386ec2b759b3a3290ee
NitinPant16 <50547525+NitinPant16@users.noreply.github.com>
2024-02-29 16:31:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('It's Worked. CI/CD works on Instance Test Server')
+  res.send('Its Worked. CI/CD works on Instance Test Server')
 })
 
 app.listen(port, () => {
