f53c2ba3dfcf663ee4e215a569f8ebad4c4ff3c8
NitinPant16 <50547525+NitinPant16@users.noreply.github.com>
2024-09-24 14:35:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Its Worked. Jenkins Pipeline on GKE are working CI/CD works With LoadBalancer')
+  res.send('Its Worked. Jenkins Pipeline on GKE are working CI/CD works With LoadBalancer and deployment strategy')
 })
 
 app.listen(port, () => {
